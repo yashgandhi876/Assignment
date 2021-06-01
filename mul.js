f4213dec39ff29773a0e589aae7b0d8437aac497
yash gandhi <yashgandhi2020@gmail.com>
2021-06-01 14:39:43 +0530

    temp

--- mul.js
+++ mul.js
@@ -1,7 +1,8 @@
-function multiply(num1=0, num2=0){ // yash gandhi here       
+function multiply(num1=0, num2=0){ // yash gandhi here
     let temp = num1*num2;
     if(temp < 0){
         temp *= -1;
     }
     console.log(temp);
 }
+//here we go
